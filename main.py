8989fcb9bf9e224167be02e62869bf90f53a3dc8
Abhinn Vyas <abhinnvyas2@gmail.com>
2022-06-08 14:59:08 -0700

    Dont know what changed

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ship import Enemy, Player
 import os
 import random
 
-pygame.font.init()c
+pygame.font.init()
 
 NORMAL_FONT = pygame.font.SysFont('comicsans', 40, True)
 WIN_OVER_FONT = pygame.font.SysFont('comicsans', 100)
